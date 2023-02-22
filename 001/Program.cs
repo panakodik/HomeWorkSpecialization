c7f28a971233fbb47ccc73613277d519f0d3711b
Aleksandr <pandom@yandex.ru>
2023-02-23 01:24:27 +0300

    задал новый массив с длинной оригинала

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,3 +1,5 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <=3 символам
 Console.WriteLine("Имеющийся массив строк");
 string[] originalStrings = { "driver", "Home", "window", "you", "my", "car" };
+string[] shortStrings = new string[originalStrings.Length];
+int count = 0;
